c99fd872bb653a041515818d0b481580c2b07c5b
Your Name <you@example.com>
2024-09-14 16:09:56 +0300

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000; //add your port here
 const PUBLISHABLE_KEY = "pk_test_51OToiZElZ1O2ZihGsTBUEvxU52lMPX4KY0nmxueq4x6Zk93a23hJGvTv7OoMl5QxSn4baSJOvkCexrNHzgqVDr1500MOmUOUuo";
 const SECRET_KEY = "sk_test_51OToiZElZ1O2ZihGoeh2GvfKAfX87lyLS4J3lNaAsdSJRpOYJF0lsAb9a93nGtqqhJHbjEWtPsrPDiLdLmmLh8bP004jfeM2b9";
- 
+
 import Stripe from "stripe";
 
 //Confirm the API version from your stripe dashboard
@@ -19,7 +19,7 @@ app.listen(port, () => {
 app.post("/create-payment-intent", async (req, res) => {
   try {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: 10000, 
+      amount: 10000,
       currency: "usd",
     });
 
@@ -42,7 +42,7 @@ app.use(bodyParser.json());
 
 
 app.post('/saveMarriage', async (req, res) => {
-  const { girlName, girlDob, manName, manDob, firstDowry, lastDowry, notes, firstWitness, secondWitness, userId } = req.body;
+  const { girlName, girlDob, manName, manDob, firstDowry, lastDowry, notes, firstWitness, secondWitness, date, userId } = req.body;
 
   try {
     const marriage = await prisma.marriage.create({
@@ -56,6 +56,7 @@ app.post('/saveMarriage', async (req, res) => {
         notes,
         firstWitness,
         secondWitness,
+        date,
         userId,
       },
     });
@@ -87,5 +88,48 @@ app.get('/getMarriages', async (req, res) => {
   } catch (error) {
     console.error("Error fetching marriages:", error);
     res.status(500).json({ success: false, error: "Failed to fetch marriages" });
+  }
+});
+
+
+
+
+app.post('/saveDivorce', async (req, res) => {
+  const { girlName, girlDob, girlRN, manName, manDob, manRN, notes, firstWitness, firstWitnessRN, secondWitness, secondWitnessRN, date, userId } = req.body;
+
+  try {
+    const divorce = await prisma.divorce.create({
+      data: {
+        girlName, girlDob, girlRN, manName, manDob, manRN, notes, firstWitness, firstWitnessRN, secondWitness, secondWitnessRN, date, userId
+      },
+    });
+    res.status(200).json({ success: true, divorce });
+  } catch (error) {
+    console.error("Error saving divorce:", error);
+    res.status(500).json({ success: false, error: "Failed to save divorce" });
+  }
+});
+
+
+
+app.get('/getDivorces', async (req, res) => {
+  const { userId } = req.query; // Get userId from query parameters
+
+  try {
+    // Retrieve all marriages associated with the given userId
+    const divorce = await prisma.divorce.findMany({
+      where: {
+        userId: userId, // Filter by userId
+      },
+    });
+
+    if (divorce.length === 0) {
+      return res.status(404).json({ success: false, message: "No divorce found for this user" });
+    }
+
+    res.status(200).json({ success: true, divorce });
+  } catch (error) {
+    console.error("Error fetching divorce:", error);
+    res.status(500).json({ success: false, error: "Failed to fetch divorce" });
   }
 });
